9a0d04ef53c6207b6ce3af583fdf0781e51fa787
niemczus <niemczus@gmail.com>
2022-02-10 13:37:29 +0100

    Added parsing JSON Data

--- Users List/UsersVC.swift
+++ Users List/UsersVC.swift
@@ -13,9 +13,48 @@ class UsersVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("ohai")
+        getUsers()
+    }
+    
+    func getUsers() {
+        // https://jsonplaceholder.typicode.com
+        
+        guard let url = URL(string: "https:jsonplaceholder.typicode.com/users") else { return }
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: url) { data, response, error in
+            if let httpResponse = response as? HTTPURLResponse {
+                print(httpResponse.statusCode)
+            }
+            if let error = error {
+                print(error)
+            } else if let data = data {
+                print(data)
+                
+//                let dataString = String(data: data, encoding: .utf8)
+//                print(dataString ?? "no data")
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                    
+                } catch let err {
+                    print(err)
+                }
+                
+                
+            }
+        }
+        
+        dataTask.resume()
+                      
         
     }
+    
+    
+    
+    
+    
+    
 
     // MARK: - Table view data source
 
