9ec93f44431ba88ceacd1653006df31f46089694
niemczus <niemczus@gmail.com>
2022-02-11 09:40:57 +0100

    Finished project

---
+++ Users List/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Users List
+//
+//  Created by Kamil Niemczyk on 11/02/2022.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let city: String
+}
--- Users List/UsersVC.swift
+++ Users List/UsersVC.swift
@@ -10,6 +10,7 @@ import UIKit
 class UsersVC: UITableViewController {
     
     let temporaryUsers = ["Jannie", "Jessica", "Kyle", "Kam", "Steven"]
+    var users = [User]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +18,6 @@ class UsersVC: UITableViewController {
     }
     
     func getUsers() {
-        // https://jsonplaceholder.typicode.com
         
         guard let url = URL(string: "https:jsonplaceholder.typicode.com/users") else { return }
         
@@ -29,19 +29,38 @@ class UsersVC: UITableViewController {
             if let error = error {
                 print(error)
             } else if let data = data {
-                print(data)
+//                print(data)
                 
 //                let dataString = String(data: data, encoding: .utf8)
 //                print(dataString ?? "no data")
+                
                 do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: [])
-                    print(json)
+                    guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? [[String : Any]] else { return }
+                    
+                   
+        
+                    for userDictionary in json {
+                        
+                        guard
+                            let name = userDictionary["name"] as? String,
+                            let address = userDictionary["address"] as? [String : Any],
+                            let city = address["city"] as? String
+//                            let geo = address["geo"] as? [String : Any],
+//                            let lat = geo["lat"] as? String,
+//                            let lng = geo["lng"]
+                        else { return }
+                        
+                        let user = User(name: name, city: city)
+                        self.users.append(user)
+                        
+                        DispatchQueue.main.async {
+                            self.tableView.reloadData()
+                        }
+                    }
                     
                 } catch let err {
                     print(err)
                 }
-                
-                
             }
         }
         
@@ -59,16 +78,21 @@ class UsersVC: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return temporaryUsers.count
+        return users.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = UITableViewCell()
-        let user = temporaryUsers[indexPath.row]
-       
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        let user = users[indexPath.row]
+//        Old way...
+//        cell.textLabel?.text = user.name
+//        cell.detailTextLabel?.text = user.city
+        
         var content = cell.defaultContentConfiguration()
-        content.text = user
+        content.text = user.name
+        content.secondaryText = user.city
+        content.image = UIImage.init(systemName: "\(indexPath.row + 1).circle")
         cell.contentConfiguration = content
         
         return cell
